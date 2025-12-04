3c57eecc4fed57078c2738e426300e4d87efe5c2
code-deohyeon <kjk110168@gmail.com>
2025-12-04 16:32:43 +0900

    TOP3 tlqkf update

--- src/components/Home.js
+++ src/components/Home.js
@@ -30,7 +30,7 @@ function Home({user, setUser}) {
             });
             
             const boards = response.data || [];
-            const recentBoards = boards.sort((a, b) => {return b.likeCount - a.likeCount});
+            const recentBoards = boards.sort((a, b) => {return b.likeCount - a.likeCount}).slice(3);
             console.log(recentBoards);
             
             setStats({
